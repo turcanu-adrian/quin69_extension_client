26b1077ea631f0ae14796a95ad4c21325327c1cd
Gusky <vasile-adrian.turcanu@student.tuiasi.ro>
2022-07-21 17:24:34 +0300

    initial release with live, stlal, no stall notifications

---
+++ background.js
@@ -0,0 +1,61 @@
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.storage.local.set({isStalling: 'false', streamStarted: 'false'});
+});
+
+chrome.alarms.create('testAlarm', {
+	periodInMinutes: 0.1
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "testAlarm") {
+		console.log("ALARM ACTIVATED! FETCHING DATA...");
+		fetch("http://localhost:3000/") 
+			.then(response=>response.text()) 
+			.then(data=>{
+				if (data!=""){
+					data = data.split('\n');
+					chrome.storage.local.get(["isStalling", "streamStarted"], function(res) {
+						if (data[1] == 'true' && res.streamStarted == 'false')
+							chrome.storage.local.set({streamStarted:'true'}, function(){
+								console.log("STREAMER LIVE NOTIFICATION");
+								chrome.notifications.create('test', {
+									type: 'basic',
+									iconUrl: '/images/pogu64.png',
+									title: 'STREAMER IS NOW LIVE',
+									message: 'WATCH NOW',
+									priority: 2
+								});
+							});
+						else if (data[1]=='false'){
+							chrome.storage.local.set({streamStarted:'false'}, function(){
+								if (data[0] != res.isStalling){
+									chrome.storage.local.set({isStalling: data[0]}, function(){
+										if (data[0]=='true'){
+											console.log("CREATING STALL NOTIFICATION");
+											chrome.notifications.create('test', {
+											type: 'basic',
+											iconUrl: '/images/painchamp64.png',
+											title: 'STREAMER IS NOW STALLING',
+											message: 'They are Just Chatting',
+											priority: 2
+											});
+										}
+										else{
+											console.log("CREATING NOT STALL NOTIFICATION");
+											chrome.notifications.create('test', {
+											type: 'basic',
+											iconUrl: '/images/pogu64.png',
+											title: 'STREAMER IS NOW NOT STALLING',
+											message: 'They are now playing '+data[2],
+											priority: 2
+											});
+										}
+									});
+								}
+							});
+						}
+					});
+				}
+			});
+		}
+});
---
+++ popup.js
@@ -0,0 +1,7 @@
+//https://api.twitch.tv/helix/streams?user_login=quin69
+
+/*
+    <a href="https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=mstrgjzmmtwisyhgcxghh03a4bg9s6&redirect_uri=http://localhost:3000" target="_blank">CONNECT WITH TWITCH</a>
+*/
+
+
